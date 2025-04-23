0616cf2d61dcf587b24f77e53cc78ac6dc6370a5
Darlington720 <70294454+Darlington720@users.noreply.github.com>
2025-04-23 18:29:06 +0300

    Added SSL

--- src/app/configs/apiConfig.js
+++ src/app/configs/apiConfig.js
@@ -1,7 +1,7 @@
 // const mainUrl =
 //   "http://ec2-3-147-67-81.us-east-2.compute.amazonaws.com:2323/graphql";
 const mainUrl = "https://tredumo.com/graphql";
-const url = "http://localhost:8000";
+const url = "https://tredumo.com/cid_api";
 const webSocketUrl = "ws://tredumo.com/graphql";
 const url2 = "https://tredumo.com/api";
 
